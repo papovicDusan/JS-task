19848084b012b2470db70b93cd90c120320275d1
Dusan Papovic <dusanpapovic@Dusans-MacBook-Pro.local>
2022-02-02 13:19:20 +0100

    anonimne funkcije

---
+++ js-anonymous-function.js
@@ -0,0 +1,17 @@
+// task 1
+const numbers = [4, 9, 16, 25];
+const newNumbers=numbers.map(x=>x*x);
+console.log('newNumbers',newNumbers);
+
+// task 2
+IIFE = (x,y) =>{
+    let sum= x+y;
+    console.log('sum',sum);
+}
+IIFE(5,10);
+
+//task 3
+
+timesTwo=(number)=> number*2;
+let numberTimesTwo=timesTwo(10);
+console.log(numberTimesTwo);
