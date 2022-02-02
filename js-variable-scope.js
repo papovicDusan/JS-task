5d838d833ad8889d8f6e50f3a8f05b4f2eefc6f6
Dusan Papovic <dusanpapovic@Dusans-MacBook-Pro.local>
2022-02-02 13:20:28 +0100

    anonimne funkcije

--- js-variable-scope.js
+++ js-variable-scope.js
@@ -1,30 +1,27 @@
-<<<<<<< Updated upstream
-=======
 
 
 // task 1 and 2
 function scopeFunction1() {
 
     for (let i = 0; i < 5; i++) {
-        var x=10;
-      }
-      console.log('var x',x);
-      console.log('let i',i);
-  }
+        var x = 10;
+    }
+    console.log('var x', x);
+    console.log('let i', i);
+}
 
-  // task 3
-  let y=5;
-  function scopeFunction3() {
-    console.log('y',y);
-  }
+// task 3
+let y = 5;
+function scopeFunction3() {
+    console.log('y', y);
+}
 
-   // task 4
-   function scopeFunction4() {
-    let z=15;
-  }
-  console.log('z',z);
+// task 4
+function scopeFunction4() {
+    let z = 15;
+}
+console.log('z', z);
 
 //scopeFunction1();
 //scopeFunction3();
  // scopeFunction4();
->>>>>>> Stashed changes
